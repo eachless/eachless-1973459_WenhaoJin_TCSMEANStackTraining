a996657234d32f4837b412a3a4e1102f44045365
wenhao jin <eachless@gmail.com>
2021-04-15 20:54:06 -0400

    revised

--- CallRecordDataProject/app.js
+++ CallRecordDataProject/app.js
@@ -8,18 +8,18 @@ fs.readFile('call_data.json',(err,data)=>{
 
 
 
-// let mongoClient =require("mongodb").MongoClient
-// let url ="mongodb://localhost:27017";
-// mongoClient.connect(url,{useUnifiedTopology:true},(err,client)=>{
-//     if(!err){
-//         let db  =client.db("meanstack");
-//         db.collection("projectData").insertOne({jasonParsed},(err2,result)=>{
-//             if(!err2){
-//                 console.log(result.insertedCount);
-//             }else{
-//                 console.log(err2.message);
-//             }
-//             client.close();
-//         })
-//     }
-// })
+let mongoClient =require("mongodb").MongoClient
+let url ="mongodb://localhost:27017";
+mongoClient.connect(url,{useUnifiedTopology:true},(err,client)=>{
+    if(!err){
+        let db  =client.db("meanstack");
+        db.collection("projectData").insertOne({jasonParsed},(err2,result)=>{
+            if(!err2){
+                console.log(result.insertedCount);
+            }else{
+                console.log(err2.message);
+            }
+            client.close();
+        })
+    }
+})
