65bbb7bd3116a711d79e027c29bad962ee8d2758
wenhao jin <eachless@gmail.com>
2021-04-18 23:20:46 -0400

    revised new one

--- NodeSocketProject/server.js
+++ NodeSocketProject/server.js
@@ -1,6 +1,11 @@
 var app =require("express")();
 let http =require('http').Server(app);
 let io = require("socket.io")(http);
+let mongoClient = require("mongodb").MongoClient;
+let url  ="mongodb://localhost:27017";
+
+
+var storeName ,storedMes;
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/index.html");
 })
@@ -9,14 +14,27 @@ app.get("/",(req,res)=>{
 io.on("connection",(socket)=>{
     console.log("Client connect to application");
     
-    socket.on("name",(msg)=>{
-        console.log("hello" + msg);
+    socket.on("name",(msg,mess)=>{
+        console.log("hello" + msg +"aaaaa " +  mess) ;
+        storeName = msg;
+        mongoClient.connect(url, {useUnifiedTopology: true },(err1,client)=>{
+            if(!err1){
+                let db =client.db("meanstack");
+                db.collection("socketMessage").insertOne({sname:storeName,messageName:mess},(err2,result)=>{
+                    if(!err2){
+                        console.log(result.insertedCount);
+                    }else{
+                        console.log(err2.message);
+                    }
+                    client.close
+                })
+            }
+        });
     })
-    socket.on("mmm",(mess)=>{
-        console.log("your" + mess);
-    })
-
 })
+
+
+
 
 
 
